3631513f3caa92e973c952091a6d730d868f183d
Roberson Andrade <roberson_souza11@hotmail.com>
2022-03-16 10:11:34 -0300

    Basic project structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
 
 function App() {
-  
-}
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate replace to='/login'/>}/>
+      <Route path='/login' element={<div>login</div>}/>
+      <Route path='/home' element={<div>home</div>}/>
+      <Route path='*' element={<div>Page not found 404</div>}/>
+    </Routes>
+  );
+};
 
 export default App
